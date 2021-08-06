a3c88d0dd8614efaff9dfdebbc237d43be2379ec
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-08-06 20:20:17 +0530

    CRUD for posts

--- meowk/blog/views.py
+++ meowk/blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .models import Post
+from .models import Post, Blogger
+from .forms import PostForm
 
 
 def home(request):
@@ -20,6 +21,45 @@ def post(request, pk):
     return render(request, 'blog/post.html', context)
 
 
-# def blog(request):
-    #     context = {}
+def create_post(request):
+
+    form = PostForm()
+    if request. method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {
+        'form': form
+    }
+    return render(request, 'blog/create-post.html', context)
+
+
+def update_post(request, pk):
+    post = Post.objects.get(id=pk)
+    form = PostForm(instance=post)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'blog/update-post.html', context)
+
+
+def delete_post(request, pk):
+    post = Post.objects.get(id=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect('home')
+
+    return render(request, 'blog/delete-post.html')
+# def blogger(request, pk):
+#     blogger = Blogger.objects.get(id=pk)
+#     context = {
+#         'blogger': blogger
+#     }
 #     return render(request, 'blog/blog.html', context)
